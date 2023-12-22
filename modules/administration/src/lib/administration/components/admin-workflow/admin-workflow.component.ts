e3867f3247b378adb14b6485b76011ba9e14087e
nicopastor <nicopastor@users.noreply.github.com>
2023-12-22 15:07:25 +0100

    Gdd 2 (#3)
    * full update
    
    * feat: workflow table
    
    * fix: style + rename workflow table component
    
    ---------
    
    Co-authored-by: Guillaume <gdurandiere-ext@almond.consulting>
    Co-authored-by: Nicolas PASTOR <nicolas.pastor@ouidou.fr>

--- modules/administration/src/lib/administration/components/admin-workflow/admin-workflow.component.ts
+++ modules/administration/src/lib/administration/components/admin-workflow/admin-workflow.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Workflow, WorkflowHttpService } from '@te44-front/shared';
 import { ButtonModule } from 'primeng/button';
