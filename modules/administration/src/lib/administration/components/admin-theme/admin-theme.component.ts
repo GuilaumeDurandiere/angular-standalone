d324b2d1a4106854c3a440db16bc006c16d906dc
Guillaume <gdurandiere-ext@almond.consulting>
2023-12-27 16:49:43 +0100

    add admin theme

---
+++ modules/administration/src/lib/administration/components/admin-theme/admin-theme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-theme',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-theme.component.html',
+  styleUrl: './admin-theme.component.less',
+})
+export class AdminThemeComponent {}
