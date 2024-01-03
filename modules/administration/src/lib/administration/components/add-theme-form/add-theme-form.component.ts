e3b241927a0abecb1292c8469f0bda56ec93c0fe
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-03 09:48:04 +0100

    update

--- modules/administration/src/lib/administration/components/add-theme-form/add-theme-form.component.ts
+++ modules/administration/src/lib/administration/components/add-theme-form/add-theme-form.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { FormControlPresenterComponent, FormGroupPresenterComponent, SubThemeForm, ThemeForm } from '@te44-front/shared';
+import { FormControlPresenterComponent, FormGroupPresenterComponent, SubThemeForm, SubThemeFormValue, ThemeForm, ThemeFormValue } from '@te44-front/shared';
 import { PrimeIcons } from 'primeng/api';
 import { InputTextModule } from 'primeng/inputtext';
+import { tap } from 'rxjs';
 import { AddSubthemeFormComponent } from '../add-subtheme-form/add-subtheme-form.component';
 
 @Component({
@@ -15,30 +16,30 @@ import { AddSubthemeFormComponent } from '../add-subtheme-form/add-subtheme-form
 })
 export class AddThemeFormComponent {
 
-  formGroup: FormGroup<ThemeForm> = this.formBuilder.group<ThemeForm>({
+  formGroup: FormGroup<ThemeForm> = this.formBuilder.nonNullable.group<ThemeForm>({
     icon: new FormControl<PrimeIcons>(PrimeIcons.ALIGN_CENTER, { nonNullable: true, validators: [Validators.required] }),
     name: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
     description: new FormControl<string>('', { nonNullable: true }),
     status: new FormControl<boolean>(true, { nonNullable: true, validators: [Validators.required] }),
-    subtheme: this.formBuilder.array([]),
+    subtheme: new FormArray<FormGroup<SubThemeForm>>([]),
   })
 
-  get formSubtheme(): FormArray {
+  get formSubtheme(): FormArray<FormGroup<SubThemeForm>> {
     return this.formGroup.controls.subtheme;
   }
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  addSubTheme(): FormGroup<SubThemeForm> {
-    return this.formBuilder.group<SubThemeForm>({
-      name: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
-      description: new FormControl<string>('', { nonNullable: true }),
-    })
+  constructor(private formBuilder: FormBuilder) {
+    this.subscribeToValueChange();
   }
 
   onAddSubTheme(): void {
-    this.formGroup.controls.subtheme.push(this.addSubTheme());
+    this.formGroup.controls.subtheme.push(new FormControl<SubThemeFormValue>({ name: '', descritpion: '' }, { nonNullable: true }));
   }
 
+  subscribeToValueChange(): void {
+    this.formGroup.valueChanges.pipe(
+      tap((themeValue: Partial<ThemeFormValue>) => console.log(themeValue))
+    ).subscribe();
+  }
 
 }
