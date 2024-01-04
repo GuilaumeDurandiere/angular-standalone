0237da43500a59f1a725986395ba15bbe1edf603
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-04 08:59:47 +0100

    update http

--- modules/shared/src/services/theme-http.service.ts
+++ modules/shared/src/services/theme-http.service.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { parseResponse } from "../helpers/zod.helper";
+import { ThemeCreateDto } from "../models/ThemeCreateDto";
+import { ThemeUpdateDto } from "../models/ThemeUpdateDto";
+import { Theme, ThemeSchema } from "../zod/Theme.zod";
 import { BaseHttpService } from "./base/base-http.service";
 
 @Injectable({
@@ -12,4 +17,19 @@ export class ThemeHttpService extends BaseHttpService {
     super('theme');
   }
 
+  get(id: number): Observable<Theme> {
+    return this.http.get<Theme>(`${this.apiUrl}/${id}`).pipe(parseResponse(ThemeSchema));
+  }
+
+  create(stepCreateDto: ThemeCreateDto): Observable<Theme> {
+    return this.http.post<Theme>(`${this.apiUrl}/create`, stepCreateDto).pipe(parseResponse(ThemeSchema));
+  }
+
+  update(stepUpdateDto: ThemeUpdateDto): Observable<Theme> {
+    return this.http.put<Theme>(`${this.apiUrl}/update`, stepUpdateDto).pipe(parseResponse(ThemeSchema));
+  }
+
+  delete(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.apiUrl}/${id}`);
+  }
 }
