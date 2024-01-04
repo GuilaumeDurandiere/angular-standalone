1e4793f70fef469976abaa9d58905a9eb4126742
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-04 11:52:14 +0100

    update modal add button

--- modules/administration/src/lib/administration/components/modal-add-theme/modal-add-theme.component.ts
+++ modules/administration/src/lib/administration/components/modal-add-theme/modal-add-theme.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { AddThemeFormComponent } from '../add-theme-form/add-theme-form.component';
 
 @Component({
@@ -8,8 +10,20 @@ import { AddThemeFormComponent } from '../add-theme-form/add-theme-form.componen
   imports: [
     AddThemeFormComponent,
     CommonModule,
+    ButtonModule,
   ],
   templateUrl: './modal-add-theme.component.html',
   styleUrl: './modal-add-theme.component.less',
 })
-export class ModalAddThemeComponent { }
+export class ModalAddThemeComponent {
+
+  constructor(private ref: DynamicDialogRef) { }
+
+  cancel(): void {
+    this.ref.close()
+  }
+
+  validate(): void {
+    this.ref.close()
+  }
+}
