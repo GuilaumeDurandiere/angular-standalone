d6406a43b41fe9de2c20b481ccd57d2539d3a4c6
Nicolas PASTOR <nicolas.pastor@ouidou.fr>
2024-01-08 09:25:04 +0100

    feat: header bar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { NgxsModule } from '@ngxs/store';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -27,5 +28,6 @@ export const appConfig: ApplicationConfig = {
     PRIMENG_SERVICES,
     provideRouter(appRoutes),
     provideHttpClient(),
+    provideAnimations()
   ],
 };
