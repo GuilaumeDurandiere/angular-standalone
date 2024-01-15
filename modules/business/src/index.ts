9339609df8739b3fa90d55ff341e3b1ca46dc5be
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-15 17:07:36 +0100

    update routing

--- modules/business/src/index.ts
+++ modules/business/src/index.ts
@@ -1,4 +1,4 @@
 import '@angular/localize/init';
 
 export * from './lib/lib.routes';
-export * from './lib/business/business.component';
+
--- modules/business/src/lib/lib.routes.ts
+++ modules/business/src/lib/lib.routes.ts
@@ -1,8 +1,8 @@
 import { Route } from '@angular/router';
-import { BusinessComponent } from './business/business.component';
+import { BusinessLandingComponent } from './landing/business-landing/business-landing.component';
 import { BusinessNewLandingComponent } from './landing/business-new-landing/business-new-landing.component';
 
 export const businessRoutes: Route[] = [
-  { path: '', component: BusinessComponent },
+  { path: '', component: BusinessLandingComponent },
   { path: 'new', component: BusinessNewLandingComponent },
 ];
