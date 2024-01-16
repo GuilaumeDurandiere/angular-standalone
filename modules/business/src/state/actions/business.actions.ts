e9e17d2ed111688bf9663c087ff2d4fff9d6a2ef
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-16 09:50:59 +0100

    add business state

---
+++ modules/business/src/state/actions/business.actions.ts
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace BusinessStateActions {
+
+  const prefixAction = '[BusinessState]';
+
+  export class InitThemes {
+    static readonly type = `${prefixAction} InitThemes`;
+  }
+}
+
+
+
---
+++ modules/business/src/state/business.state.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { Subtheme, SubthemeHttpService, Theme, ThemeHttpService } from '@te44-front/shared';
+import { tap } from 'rxjs';
+import { BusinessStateActions } from './actions/business.actions';
+import { BusinessStateModel } from './models/business-state.model';
+
+export const initBusinessStateModel: BusinessStateModel = {
+  themes: [],
+  subthemes: [],
+};
+
+@State<BusinessStateModel>({
+  name: 'Business',
+  defaults: initBusinessStateModel,
+})
+@Injectable()
+export class BusinessState {
+
+  constructor(
+    private themeHttpService: ThemeHttpService,
+    private subthemeHttpService: SubthemeHttpService
+  ) { }
+
+  /*************
+   * SELECTORS *
+   *************/
+
+  @Selector()
+  static getThemes(state: BusinessStateModel): Theme[] {
+    return state.themes
+  }
+
+  @Selector()
+  static getSubtheme(state: BusinessStateModel): Subtheme[] {
+    return state.subthemes
+  }
+
+  /***********
+   * Actions *
+   ***********/
+
+  @Action(BusinessStateActions.InitThemes)
+  initTheme(ctx: StateContext<BusinessStateModel>) {
+    return this.themeHttpService.getAll().pipe(
+      tap((themes: Theme[]) => ctx.patchState({ themes }))
+    )
+  }
+
+}
