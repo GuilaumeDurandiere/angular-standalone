b46fdeb3fb6147afbaed13fd30d67c92d47c31ba
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-18 16:48:26 +0100

    GDD-29 first version of modal

--- modules/business/src/lib/components/modal-new-business/modal-new-business.component.ts
+++ modules/business/src/lib/components/modal-new-business/modal-new-business.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControlPresenterComponent, FormGroupPresenterComponent } from '@te44-front/shared';
+import { ButtonModule } from 'primeng/button';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-modal-new-business',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    FormControlPresenterComponent,
+    FormGroupPresenterComponent,
+    InputMaskModule,
+    InputTextModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './modal-new-business.component.html',
   styleUrl: './modal-new-business.component.less',
 })
-export class ModalNewBusinessComponent {}
+export class ModalNewBusinessComponent {
+
+  formGroup: FormGroup = this.formBuilder.group({
+    message: new FormControl<string>(''),
+    telephone: new FormControl<string | null>(null, { validators: Validators.required })
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private ref: DynamicDialogRef
+  ) { }
+
+  cancel(): void {
+    this.ref.close();
+  }
+
+  validate(): void {
+    this.formGroup.updateValueAndValidity();
+    this.formGroup.markAllAsTouched();
+    this.formGroup.markAsTouched();
+    this.formGroup.markAsDirty();
+    if (this.formGroup.valid) {
+      this.ref.close(this.formGroup.getRawValue());
+    }
+  }
+}
