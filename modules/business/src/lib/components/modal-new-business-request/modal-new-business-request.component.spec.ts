77f427a5550f8180bab144f62376722f4e8a10f6
GuilaumeDurandiere <guillaume.durandiere@orange.fr>
2024-01-24 09:28:58 +0100

    add modal (#25)
    Co-authored-by: Guillaume <gdurandiere-ext@almond.consulting>

---
+++ modules/business/src/lib/components/modal-new-business-request/modal-new-business-request.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModalNewBusinessRequestComponent } from './modal-new-business-request.component';
+
+describe('ModalNewBusinessRequestComponent', () => {
+  let component: ModalNewBusinessRequestComponent;
+  let fixture: ComponentFixture<ModalNewBusinessRequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ModalNewBusinessRequestComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ModalNewBusinessRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ modules/business/src/lib/components/modal-new-business-request/modal-new-business-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal-new-business-request',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal-new-business-request.component.html',
+  styleUrl: './modal-new-business-request.component.less',
+})
+export class ModalNewBusinessRequestComponent {}
