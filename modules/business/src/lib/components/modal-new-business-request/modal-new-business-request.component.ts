f9046903de6cef62180d226fd99d4a3c9e6393c3
Guillaume <gdurandiere-ext@almond.consulting>
2024-01-25 16:19:10 +0100

    GDD-52 add modal emprise to form

--- modules/business/src/lib/components/modal-new-business-request/modal-new-business-request.component.ts
+++ modules/business/src/lib/components/modal-new-business-request/modal-new-business-request.component.ts
@@ -4,9 +4,11 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 import { BusinessRequestForm, BusinessRequestFormValue, FormControlPresenterComponent } from '@te44-front/shared';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
-import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { take } from 'rxjs';
+import { ModalAddEmpriseComponent } from '../modal-add-emprise/modal-add-emprise.component';
 
 @Component({
   selector: 'app-modal-new-business-request',
@@ -16,8 +18,7 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
   styleUrl: './modal-new-business-request.component.less',
 })
 export class ModalNewBusinessRequestComponent {
-  fieldStyle = { 'width': '25rem' };
-  isHorsTravaux: boolean = this.config.data.isHorsTravaux;
+
   formGroup: FormGroup<BusinessRequestForm> = this.formBuilder.group<BusinessRequestForm>({
     nom: new FormControl<string>('', { validators: Validators.required, nonNullable: true }),
     prenom: new FormControl<string>('', { validators: Validators.required, nonNullable: true }),
@@ -29,11 +30,41 @@ export class ModalNewBusinessRequestComponent {
     description: new FormControl<string>('', { validators: Validators.required, nonNullable: true }),
   });
 
+  fieldStyle = { 'width': '25rem' };
+  isHorsTravaux: boolean = this.config.data.isHorsTravaux;
+
+  points: { lat: number, lng: number }[] = [];
+
+  dialog: DynamicDialogRef | null = null;
+
+
   constructor(
+    private config: DynamicDialogConfig,
+    private dialogService: DialogService,
     private formBuilder: FormBuilder,
     private ref: DynamicDialogRef,
-    private config: DynamicDialogConfig,
   ) { }
+
+  openModalAddEmprise(points: { lat: number, lng: number }[] | null): void {
+    this.dialog = this.dialogService.open(ModalAddEmpriseComponent, {
+      header: $localize`:@@ADD_THEME_TITLE:Ajouter un thème`,
+      height: '80%',
+      width: '60%',
+      maximizable: true,
+      closeOnEscape: false,
+      data: {
+        points,
+      }
+    });
+
+    this.dialog.onClose
+      .pipe(
+        take(1),
+      )
+      .subscribe((formValue: { image: string, points: { lat: number, lng: number }[] }) => {
+        this.points = formValue.points;
+      });
+  }
 
   cancel(): void {
     this.ref.close();
